af5b43817ae95c43478c4663bd2e535c108803bd
Tim <tim.esselens@gmail.com>
2010-12-11 21:14:06 +0100

    FEAT: implemented liveboard backend

--- app.psgi
+++ app.psgi
@@ -2,8 +2,7 @@
 use Plack::Builder;
 use IRail::PSGI::Stations;
 use IRail::PSGI::Connections;
-
-#sub status { [ shift, [ 'Content-Type' => 'text/plain' ], [ shift ] ] }
+use IRail::PSGI::Liveboard;
 
 builder {
     enable 'Plack::Middleware::Static', path => qr{^/(images|js|html|static|css|favicon\.ico)}, root => 'html/';
@@ -13,6 +12,10 @@ builder {
     mount '/connections/' => builder {
         #enable '+WebHive::Middleware::Cache', config => { driver => 'File', global => 1 };
         $IRail::PSGI::Connections::API;
+    };
+    mount '/liveboard/' => builder {
+        #enable '+WebHive::Middleware::Cache', config => { driver => 'File', global => 1 };
+        $IRail::PSGI::Liveboard::API;
     };
 }
 
---
+++ lib/IRail/PSGI/Liveboard.pm
@@ -0,0 +1,87 @@
+package IRail::PSGI::Liveboard;
+use strict;
+use warnings;
+use Carp;
+use Date::Format;
+use Encode;
+use HTTP::Request;
+use JSON::XS;
+use List::Util qw/max reduce/;
+use LWP::UserAgent;
+use Plack::Request;
+use WebHive::Log;
+use IRail::PSGI::Stations qw/get_station_id/;
+use XML::Twig;
+
+# ABSTRACT: PSGI interface for IRail API
+# AUTHOR: Tim Esselens <tim.esselens@gmail.com>
+
+our $VERSION = 0.001;
+
+# external API ##############################################################################
+our $API = sub {
+    my $env = shift;
+
+    my $req = new Plack::Request($env);
+    my $param = $req->parameters();
+
+    my ($station) = (($param->{station} || '') =~ m/^([\w\'\-\ ]+)$/io);
+
+    croak "station not defined" unless $station;
+
+    my ($lang) = (($param->{lang} || 'nl') =~ m/^(fr|de|en|nl)$/io);
+    my ($time) = (($param->{'time'} || time2str('%H%M',time)) =~ m/^(\d+\W?\d+)$/io); # hh-mm
+    my ($sid) = get_station_id($station);
+    my ($format) = ($param->{format} || 'xml' =~ m/^(xml|json|jsonp)$/io);
+    my ($timesel) = map { /^a/ && 'A' or /^d/ && 'D' } (($param->{timesel} || 'a') =~ m/^(a(?:rrive)?|d(?:epart)?)/io); # a(rrive) or d(epart)
+
+    my $ua = new LWP::UserAgent;
+       $ua->agent("IRail::PSGI/$VERSION");
+       $ua->timeout(10);
+
+    my $http_req = new HTTP::Request(GET => "http://www.railtime.be/mobile/SearchStation.aspx?l=$lang&tr=$time&sid=$sid&da=$timesel&p=2");
+    my $res = $ua->request($http_req);
+
+    croak "was unsuccessful in POSTing data" unless $res->is_success;
+
+    my @liveboard;
+
+    for (split /[\r\n]/, $res->decoded_content) {
+        next unless /^\s*<td/;
+        my $line = $_;
+        my %train = ( delay => 0, work => 0, platform => 'NA', changedplatform => 0 );
+        my ($epoch) = 86400 * int ( time / 86400 );
+
+        if ( $line =~ m#<font color="DarkGray">#)                   { $train{left} = 1 };
+        if ( $line =~ m#(\d+):(\d+)</a>#)                           { $train{epoch} = $epoch + (3600 * $1) + (60 * $2); 
+                                                                      $train{departs} = time2str("%Y-%m-%dT%H:%M:%SZ",$train{epoch},"ZULU"); }
+        if ( $line =~ m#&tid=(\d+)#)                                { $train{tid} = $1 };
+        if ( $line =~ m#&nbsp;(.*?)&nbsp;#)                         { $train{station} = $1; $train{stationid} = get_station_id($1); };
+        if ( $line =~ m#<font color="Red">\s*\+(\d+)'\s*</font>#)   { $train{delay} = int($1 * 60) };
+        if ( $line =~ m#<img src="/mobile/images/Work.png"#)        { $train{work} = 1 }
+        if ( $line =~ m#\[([\w]+)&nbsp;Spoor&nbsp;(\d+)\]# )        { $train{vehicle} = $1; $train{platform} = int($2); }
+        if ( $line =~ m#Spoor&nbsp;<[bf][^>]+>(\d+)</[^>]+>\]# )    { $train{changedplatform} = 1; $train{platform} = int($2); }
+
+        push @liveboard, \%train;
+    }
+
+    my ($xml) = map { '<?xml version="1.0" encoding="UTF-8"?>'.
+                      '<liveboard version="1.0" timestamp="'.time.'">'.$_.'</liveboard>' }
+                map { '<station stationid="'.$sid.'">'.$station.'</station>'.
+                      '<departures number="'.scalar @liveboard.'">'.$_.'</departures>' } 
+                reduce { $a.$b}  
+                map { qq#<departure id="0" delay="$_->{delay}">
+                              <time formatted="$_->{departs}">$_->{epoch}</time>
+                              <vehicle>$_->{vehicle}</vehicle>
+                              <platform changed="$_->{changedplatform}">$_->{platform}</platform>
+                              <station stationid="$_->{stationid}">$_->{station}</station>
+                         </departure># 
+                } @liveboard;
+
+    return [ 200, [ 'Content-Type' => 'text/xml' ], [ $xml ] ];
+
+};
+
+42;
+
+
